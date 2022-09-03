98cd9c6d6d0e770ca05644db165a12ab8a87edbd
Ismile Hossain <iamismile.dev@gmail.com>
2022-09-03 17:37:13 +0600

    chore: Setup an apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-const projectName: string = 'blog-app-graphql';
+import { ApolloServer } from 'apollo-server';
+import { typeDefs } from './schema';
+import { Query } from './resolvers';
 
-console.log(projectName);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers: {
+    Query,
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready on ${url}`);
+});
