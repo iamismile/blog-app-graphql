7dc11f31062dffaa47a0e841eb58ac6e1d29e934
Ismile Hossain <iamismile.dev@gmail.com>
2022-09-10 00:47:25 +0600

    feat: create data with prisma

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './schema';
-import { Query } from './resolvers';
+import { Mutation, Query } from './resolvers';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export interface Context {
+  prisma: PrismaClient<
+    Prisma.PrismaClientOptions,
+    never,
+    Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined
+  >;
+}
 
 const server = new ApolloServer({
   typeDefs,
   resolvers: {
     Query,
+    Mutation,
+  },
+  context: {
+    prisma,
   },
 });
 
