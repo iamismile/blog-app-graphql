e1079fe12363e555a6c320eb8e6e7382725f6906
Ismile Hossain <iamismile.dev@gmail.com>
2022-10-01 17:19:59 +0600

    feat: add me query

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -4,4 +4,9 @@ export const Query = {
   posts: (_: any, __: any, { prisma }: Context) => {
     return prisma.post.findMany({ orderBy: [{ createdAt: 'desc' }] });
   },
+
+  me: (_: any, __: any, { prisma, userInfo }: Context) => {
+    if (!userInfo) return null;
+    return prisma.user.findUnique({ where: { id: userInfo.userId } });
+  },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -3,6 +3,7 @@ import { gql } from 'apollo-server';
 export const typeDefs = gql`
   type Query {
     posts: [Post!]!
+    me: User
   }
 
   type Mutation {
